ee352f2ad43a5560251d914332d449cbfd666752
James Cockle <jimihendrixhewasgood@gmail.com>
2012-12-05 16:08:50 +1300

    Finish the signup form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+	# returns the image from gravater webpage
+	def gravatar_for(user, options = { size: 50 })
+		size = options[:size]
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravater_url="http://www.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravater_url, alt: user.name, 
+			                    width: size, height: size, class: "gravatar")
+	end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do 
+	factory :user do
+		name     "James Cockle"
+		email    "example@example.example"
+		password "foobarbaz"
+		password_confirmation "foobarbaz"
+
+	end
+end
